f37309e85629abdc40b8c134b7e1e57f6aee8d8a
harshana-lk <harshanajayaweerayt@gmail.com>
2023-05-14 11:32:51 +0530

    basic react app

---
+++ React/react-icet/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      "React Front-End"
+      <h1>React Front-End</h1>
+    </div>
+  );
+};
+
+export default HomePage;
