fd47068d5d5142fee3ab1e954078c3901d6531c3
harshana-lk <harshanajayaweerayt@gmail.com>
2023-05-15 08:17:29 +0530

    added some pages

---
+++ React/react-icet/src/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      AboutPage
+      <h1>This iS About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
